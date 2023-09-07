eaa23d77aeabcadfef8f732b614474ef9cc6cf61
suyu <neverdiffer@gmail.com>
2023-09-07 21:44:51 +0000

    frontend_suyu

---
+++ frontend/src/components/Transfer.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+export function Transfer({ transferTokens, tokenSymbol, conAddress }) {
+  return (
+    <div>
+      <h4>Transfer</h4>
+      <form
+        onSubmit={(event) => {
+          // This function just calls the transferTokens callback with the
+          // form's data.
+          event.preventDefault();
+
+          const formData = new FormData(event.target);
+          const to = formData.get("to");
+          const amount = formData.get("amount");
+
+          if (to && amount) {
+            transferTokens(to, amount);
+          }
+        }}
+      >
+        <div className="form-group">
+          <label>Amount of {tokenSymbol}</label>
+          <input
+            className="form-control"
+            type="number"
+            step="1"
+            name="amount"
+            placeholder="1"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label>Recipient address</label>
+          <input className="form-control" type="text" name="to" required />
+        </div>
+        <div className="form-group">
+          <input className="btn btn-primary" type="submit" value="Transfer" />
+        </div>
+        <div className="form-group">
+          <p>contract Address : {conAddress} </p>
+          <p>Symbol : {tokenSymbol} </p>
+          <p>Decimal : 18 </p>
+        </div>
+      </form>
+    </div>
+  );
+}
